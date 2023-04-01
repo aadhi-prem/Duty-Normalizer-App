21f2dc839f0da51a4fe6a24815d1a423d43eede1
RITHIKA KATHIRVEL <89640200+RITHIKAKATHIR@users.noreply.github.com>
2023-04-01 20:07:40 +0530

    Update add.dart
    add in batches with validation

--- lib/add.dart
+++ lib/add.dart
@@ -6,6 +6,8 @@ import 'package:file_picker/file_picker.dart';
 import 'dart:io';
 import 'dart:convert' show utf8;
 import 'DB_HELPER.dart';
+import 'dart:core';
+import 'package:email_validator/email_validator.dart';
 
 void main() => runApp(MyApp());
 
@@ -97,27 +99,26 @@ class _ToggleButtonScreenState extends State<ToggleButtonScreen> {
 
                   openFile(file,_buttonValue!);
 
-                  showDialog(
-                    context: context,
-                    builder: (ctx) =>
-                        AlertDialog(
-                          title: const Text("Pop Up"),
-                          content: const Text(
-                              "File picked and added to DB sucessfully!"),
-                          actions: <Widget>[
-                            TextButton(
-                              onPressed: () {
-                                Navigator.of(ctx).pop();
-                              },
-                              child: Container(
-                                color: Colors.blueAccent,
-                                padding: const EdgeInsets.all(14),
-                                child: const Text("ok"),
-                              ),
-                            ),
-                          ],
-                        ),
-                  );
+                  // showDialog(
+                  //   context: context,
+                  //   builder: (ctx) =>
+                  //       AlertDialog(
+                  //         title: const Text("Pop Up"),
+                  //         content: const Text("File picked sucessfully!"),
+                  //         actions: <Widget>[
+                  //           TextButton(
+                  //             onPressed: () {
+                  //               Navigator.of(ctx).pop();
+                  //             },
+                  //             child: Container(
+                  //               color: Colors.blueAccent,
+                  //               padding: const EdgeInsets.all(14),
+                  //               child: const Text("ok"),
+                  //             ),
+                  //           ),
+                  //         ],
+                  //       ),
+                  // );
                 }
               },
               child: const Text("Select File"),
@@ -147,15 +148,106 @@ class _ToggleButtonScreenState extends State<ToggleButtonScreen> {
       }
       rows.add(rowMap);
     }
+    int i = 0;
+    int flag = 0;
+    String mychar = '';
 
 
-    for (Map<String, dynamic> a in rows) {
-      await LocalDB().writeDB(a,_buttonvalue);
-      // await LocalDB().writeD
+    if(_buttonValue=='Mtech') {
+      mychar='M';
+    } else if(_buttonValue=='PhD') {
+      mychar='P';
+    } else {
+      mychar='A';
+    }
+    int count=0;
+    for (Map<String, dynamic> r in rows) {
+      if (r['Rollnumber'].startsWith(mychar)) {
+        debugPrint("YES: starts with correct letter");
+        i++;
+      }
+      else {
+        debugPrint("NO: doesnt start with correct letter");
+        flag = 1;
+      }
+      if(validateEmail(r['Email'])) {
+        debugPrint("Valid Email");
+
+      } else{
+        debugPrint("Invalid Email");
+        flag=1;
+      }
+      if(flag==1) count++;
 
     }
 
+
+
+    if (flag == 0) {
+      for (Map<String, dynamic> a in rows) {
+        debugPrint("FLAG IS TRUE");
+        await LocalDB().writeDB(a, _buttonvalue);
+        // await LocalDB().writeD
+        showDialog(
+          context: context,
+          builder: (ctx) =>
+              AlertDialog(
+                title: const Text("Pop Up"),
+                content: const Text("File picked and added to DB sucessfully!"),
+                actions: <Widget>[
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(ctx).pop();
+                    },
+                    child: Container(
+                      color: Colors.blueAccent,
+                      padding: const EdgeInsets.all(14),
+                      child: const Text("ok"),
+                    ),
+                  ),
+                ],
+              ),
+        );
+
+      }
+    }
+    else
+      {
+
+        showDialog(
+          context: context,
+          builder: (ctx) =>
+              AlertDialog(
+                title: const Text("Pop Up"),
+                content: Text("Invalid Data in $count row in CSV File. Upload the correct CSV file!"),
+                actions: <Widget>[
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(ctx).pop();
+                    },
+                    child: Container(
+                      color: Colors.blueAccent,
+                      padding: const EdgeInsets.all(14),
+                      child: const Text("ok"),
+                    ),
+                  ),
+                ],
+              ),
+        );
+
+      }
   }
+
+  bool validateEmail(String email) {
+    final emailRegex = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+    if (email.isEmpty) {
+      return false;
+    }
+    return emailRegex.hasMatch(email);
+  }
+
+
+
 }
 
 
