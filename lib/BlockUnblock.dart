27543e6ddef88c55add06c7b299996d284bd7eb1
NVNTH99 <91349204+NVNTH99@users.noreply.github.com>
2023-04-01 22:44:16 +0530

    Add files via upload

--- lib/BlockUnblock.dart
+++ lib/BlockUnblock.dart
@@ -59,7 +59,7 @@ class _HomePageState extends State<HomePage> {
   List<Map<String,dynamic>> unblockedusers=[];
   List<Map<String, dynamic>> _foundUsers = [];
   List<Map<String, dynamic>> _allUsers = [];
-  late String searchWord;
+  late String searchWord = "";
   @override
   @override
   void initState() {
@@ -81,7 +81,7 @@ class _HomePageState extends State<HomePage> {
   // This function is called whenever the text field changes
   void _runFilter(String enteredKeyword) {
     List<Map<String, dynamic>> results = [];
-    if (enteredKeyword.isEmpty) {
+    if (enteredKeyword == "") {
       // if the search field is empty or only contains white-space, we'll display all users
       results = _allUsers;
     } else {
@@ -150,13 +150,45 @@ class _HomePageState extends State<HomePage> {
             const SizedBox(
               height: 20,
             ),
-            TextField(
-              onChanged: (value) {
-                searchWord = value;
-                _runFilter(value);
-              },
-              decoration: const InputDecoration(
-                  labelText: 'Search', suffixIcon: Icon(Icons.search)),
+            Container(
+              decoration: BoxDecoration(boxShadow: [
+                BoxShadow(
+                    offset: const Offset(12, 26),
+                    blurRadius: 50,
+                    spreadRadius: 0,
+                    color: Colors.grey.withOpacity(.1)),
+              ]),
+              child: TextField(
+                // controller: textController,
+                onChanged: (value) {
+                  searchWord = value;
+                  _runFilter(searchWord);
+                } ,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Colors.grey[500]!,
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
+                  hintText: 'Search',
+                  hintStyle:
+                  const TextStyle(color: Colors.grey, fontWeight: FontWeight.w300),
+                  contentPadding:
+                  const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+                  border: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(45.0)),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey[300]!, width: 1.0),
+                    borderRadius: BorderRadius.all(Radius.circular(45.0)),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey[400]!, width: 1.5),
+                    borderRadius: BorderRadius.all(Radius.circular(45.0)),
+                  ),
+                ),
+              ),
             ),
             const SizedBox(
               height: 5,
