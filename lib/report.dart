e12fc4c4141e2de5266fceabf170266a102eba2f
RITHIKA KATHIRVEL <89640200+RITHIKAKATHIR@users.noreply.github.com>
2023-04-03 22:58:24 +0530

    Add files via upload
    added download as pdf

--- lib/report.dart
+++ lib/report.dart
@@ -1,6 +1,7 @@
 import 'dart:ffi';
 import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:open_file/open_file.dart';
 import 'DB_HELPER.dart';
 import 'dashboard.dart';
 //import 'blankpage.dart';
@@ -12,6 +13,8 @@ import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 import 'filter_delete.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
 
 class ReportPage extends StatelessWidget {
   // const DeletePage({Key? key}) : super(key: key);
@@ -142,7 +145,10 @@ class ReportState extends State<Report> {
 
             ElevatedButton(
               style: ElevatedButton.styleFrom( primary: Color(0xffff595e),),
-              onPressed: () {  },
+              onPressed: () async {
+
+                await download(results,name!,mtech,phd,faculty,dept!);
+              },
               child: const Text('Print',style: TextStyle(fontSize: 17),),
             ),
             ElevatedButton(
@@ -158,4 +164,113 @@ class ReportState extends State<Report> {
       ),
     );
   }
+
+  Future<void> download(List<Map<String,dynamic>> m, String name,int mtech,int phd, int faculty, String dept) async {
+    debugPrint("IN DOWNLOAD");
+    List<List<String>> l=[];
+    for(Map<String,dynamic>r in m){
+      debugPrint("${r["Name"]}");
+      List<String> a=[];
+      a.add(r["ID"]);
+      a.add(r["Name"]);
+     // a.add(r["DEPARTMENT"]);
+      a.add(r["PhoneNo"]);
+      a.add(r["Email"]);
+      l.add(a);
+    }
+
+    Uint8List pdf= await createPdf(l,name,mtech,phd,faculty,dept) as Uint8List;
+    await downloadPdf(pdf);
+    debugPrint("downloaded");
+  }
+
+  Future<Uint8List> createPdf(List<List<String>> data,String name,int mtech, int phd, int faculty,String dept) async{
+    final pdf = pw.Document();
+    final tableHeaders = ['ID', 'Name', 'PhoneNo', 'Email'];
+
+    var tableData=data;
+        // pdf.addPage(pw.Page(
+        // build: (context) => pw.Table.fromTextArray(
+        // headers: tableHeaders,
+        // data: tableData,
+        // ),
+        // ));
+
+    // // Split the data into chunks of 10 rows each
+    // const chunkSize = 15;
+    // final chunks = <List<List<String>>>[];
+    // for (var i = 0; i < data.length; i += chunkSize) {
+    //   final end = (i + chunkSize < data.length) ? i + chunkSize : data.length;
+    //   chunks.add(data.sublist(i, end));
+    // }
+    // // Create a new page for each chunk of data
+    // for (var chunk in chunks) {
+    //   pdf.addPage(pw.Page(
+    //     build: (context) => pw.Table.fromTextArray(
+    //       headers: tableHeaders,
+    //       data: chunk,
+    //       cellAlignment: pw.Alignment.centerLeft,
+    //       cellStyle: const pw.TextStyle(fontSize: 10),
+    //      // cellDecoration: cellDecoration,
+    //     ),
+    //   ));
+    // }
+
+    // Create a table for each chunk of data
+    const chunkSize = 15;
+    final chunks = <List<List<String>>>[];
+    for (var i = 0; i < data.length; i += chunkSize) {
+      final end = (i + chunkSize < data.length) ? i + chunkSize : data.length;
+      chunks.add(data.sublist(i, end));
+    }
+    for (var chunk in chunks) {
+      pdf.addPage(pw.Page(
+        build: (context) => pw.Column(
+          crossAxisAlignment: pw.CrossAxisAlignment.start,
+          children: [
+            pw.Text('DUTY REPORT', style: pw.TextStyle(fontSize: 10, fontWeight: pw.FontWeight.bold) ,textAlign: pw.TextAlign.center,
+            ),
+            pw.SizedBox(height: 15),
+
+
+            pw.Text('DUTY NAME: $name', style: pw.TextStyle(fontSize: 10, fontWeight: pw.FontWeight.bold)
+            ),
+            pw.SizedBox(height: 15),
+
+            pw.Text('DEPARTMENT: $dept', style: pw.TextStyle(fontSize: 10, fontWeight: pw.FontWeight.bold)
+            ),
+            pw.SizedBox(height: 15),
+
+            // pw.Text('NUMBER OF MTECH: $mtech    NUMBER OF P.HD.: $phd    NUMBER OF FACULTY: $faculty ', style: pw.TextStyle(fontSize: 10, fontWeight: pw.FontWeight.bold)
+            // ),
+            // pw.SizedBox(height: 15),
+
+            pw.Table.fromTextArray(
+              headers: tableHeaders,
+              data: chunk,
+              cellAlignment: pw.Alignment.centerLeft,
+              cellStyle: const pw.TextStyle(fontSize: 10),
+              cellHeight: 15,
+              // cellDecoration: cellDecoration,
+            ),
+          ],
+        ),
+      ));
+    }
+
+    return pdf.save();
+  }
+
+  Future<void> downloadPdf(Uint8List pdf) async {
+    // Save the PDF file to the local file system
+    Directory tempDirectory = await getTemporaryDirectory();
+    String tempPath = tempDirectory.path;
+    File file = File('$tempPath/data.pdf');
+    await file.writeAsBytes(pdf);
+    // Open the PDF file using the open_file package
+    await OpenFile.open(file.path);
+  }
+
+
+
 }
