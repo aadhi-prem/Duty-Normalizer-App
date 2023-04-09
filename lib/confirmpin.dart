96377d893440c0cba7e93205d7b3fb4745287a8c
NVNTH99 <navaneethshanavasan02@gmail.com>
2023-04-09 13:01:43 +0530

    Error fix

--- lib/confirmpin.dart
+++ lib/confirmpin.dart
@@ -18,7 +18,7 @@ class _PinEntryPageState extends State<PinEntryPage> {
     await LocalDB().executeDB("Update Mtech set WorkHours = 0;");
     await LocalDB().executeDB("Update Phd set WorkHours = 0;");
     await LocalDB().executeDB("Update Faculty set WorkHours = 0;");
-    await LocalDB().executeDB("Delete from DutyDetails;")
+    await LocalDB().executeDB("Delete from DutyDetails;");
     await LocalDB().executeDB("Delete from Duty;");
   }
 
