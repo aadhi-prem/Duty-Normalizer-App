94f488125024458f9c4f54505058ba6267360384
clanand11 <122421143+clanand11@users.noreply.github.com>
2023-04-10 17:20:48 +0530

    Update ReportGeneration.dart

--- lib/ReportGeneration.dart
+++ lib/ReportGeneration.dart
@@ -214,7 +214,7 @@ class _ReportGenerationState extends State<ReportGeneration> {
                       ElevatedButton.icon(
                         icon: Icon(Icons.arrow_back),
                         style: ElevatedButton.styleFrom(
-                          primary: Color(0xff0077b6),
+                          primary: Color(0xff9381ff),
                         ),
                         onPressed: () {
                           Navigator.of(context, rootNavigator: true).pop();
